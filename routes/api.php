b6145218ede799a78f9d109a0dc0972fc597e5f8
devyetii <ebrahim.gomaa.hg@gmail.com>
2024-07-25 13:28:18 +0200

    add admin authorization

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Auth routes
@@ -8,10 +9,17 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 
-// Product routes
+// Product user routes
 Route::middleware('auth:api')->group(function () {
     Route::get('products', [ProductController::class, 'index']);
     Route::get('products/{product}', [ProductController::class, 'show']);
+});
+
+// Admin routes
+Route::middleware('auth:api')->middleware('admin')->group(function () {
+    Route::post('users/role', [UserController::class, 'setRole']);
+
+    // Product admin routes
     Route::post('products', [ProductController::class, 'store']);
     Route::put('products/{product}', [ProductController::class, 'update']);
     Route::delete('products/{product}', [ProductController::class, 'destroy']);
